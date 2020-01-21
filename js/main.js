7604a2fc444bc72ab464549f872a88370f02d07f
PeterKellett <peterwkellett@gmail.com>
2020-01-21 18:05:55 +0000

    Chart resizing modifications made by Code Institute support - Stephen

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
     console.log("ready");
 
-    $(window).resize(function() {
-        console.log("window resized");
-        makeGraphs function();
-    });
-
+    
     
 
 
