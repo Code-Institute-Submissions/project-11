ecb5039273b49a05893a64029ae08c33353bbe8b
PeterKellett <peterwkellett@gmail.com>
2020-01-21 17:05:16 +0000

    Charts placed in containers for resizing functionality.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     console.log("ready");
 
+    $(window).resize(function() {
+        console.log("window resized");
+        makeGraphs function();
+    });
+
     
 
 
