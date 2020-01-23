b9f9a484eac1bea65f701468644a05e16c025abc
PeterKellett <peterwkellett@gmail.com>
2020-01-23 16:01:44 +0000

    Modified chart var names so as not to be using duplicate names.
    Fixes PeterKellett/project-2#9

--- js/main.js
+++ js/main.js
@@ -23,13 +23,10 @@ $(document).ready(function () {
                 d.date = parseDate(d.date); 
         });
 
-             work();//Calls the function 'work' declared below
+             work();
         }
         
         function work(){
-
-
-     
         var name_dim = ndx.dimension(dc.pluck('name'));
         var start = name_dim.group().reduceSum(function (d) {
             if (d.squad === 1) {
@@ -47,9 +44,9 @@ $(document).ready(function () {
             }
         });
 
-        var stackedChart = dc.barChart("#attendance-chart");
-        stackedChart
-            
+        var attendanceStackedChart = dc.barChart("#attendance-chart");
+        attendanceStackedChart
+            .width($(attendanceStackedChart.anchor()).parent().width())
             .dimension(name_dim)
             .group(start, "Start")
             .stack(sub, "Sub")
@@ -60,14 +57,14 @@ $(document).ready(function () {
             .legend(dc.legend().x(555).y(10).itemHeight(15).gap(5))
             .margins({top: 30, right: 50, bottom: 30, left: 50})
 
-            
-        var name_dim = ndx.dimension(dc.pluck('name'));
-        var total_goals_per_person = name_dim.group().reduceSum(dc.pluck('goals'));
-        var total_assists_per_person = name_dim.group().reduceSum(dc.pluck('assists'));
+            var name_dim = ndx.dimension(dc.pluck('name'));
+            var total_goals_per_person = name_dim.group().reduceSum(dc.pluck('goals'));
+            var total_assists_per_person = name_dim.group().reduceSum(dc.pluck('assists'));
 
-        var stackedChart = dc.barChart('#goals-per-person-chart');
-        stackedChart
-            .width(550)
+
+            var goalsAssistsStackedChart = dc.barChart('#goals-per-person-chart');
+            goalsAssistsStackedChart
+            .width($(goalsAssistsStackedChart.anchor()).parent().width())
             .height(200)
             .margins({top: 30, right: 50, bottom: 30, left: 50})
             .dimension(name_dim)
@@ -80,12 +77,12 @@ $(document).ready(function () {
             .legend(dc.legend().x(555).y(10).itemHeight(15).gap(5))
             .yAxis().ticks(4);
         
-        var opponent_dim = ndx.dimension(dc.pluck('opponent'));
-        var total_goals_per_opponent = opponent_dim.group().reduceSum(dc.pluck('goals'));
-        var total_assists_per_opponent = opponent_dim.group().reduceSum(dc.pluck('assists'));
+            var opponent_dim = ndx.dimension(dc.pluck('opponent'));
+            var total_goals_per_opponent = opponent_dim.group().reduceSum(dc.pluck('goals'));
+            var total_assists_per_opponent = opponent_dim.group().reduceSum(dc.pluck('assists'));
 
-        var goalBarChart= dc.barChart('#goals-per-opponent-chart');
-        goalBarChart
+            var goalBarChart= dc.barChart('#goals-per-opponent-chart');
+            goalBarChart
             .width($(goalBarChart.anchor()).parent().width())
             .height(200)
             .margins({top: 30, right: 50, bottom: 30, left: 50})
@@ -99,99 +96,97 @@ $(document).ready(function () {
             .legend(dc.legend().x(560).y(10).itemHeight(15).gap(5))
             .yAxis().ticks(4); 
 
-        
-
-        var date_dim = ndx.dimension(dc.pluck('date'));
-        var minDate = date_dim.bottom(1)[0].date;
-        var maxDate = date_dim.top(1)[0].date;
-        function spend_by_name(name) {
-            return function(d) {
-                if (d.name === name) {
-                    return d.rating;
-                } else {
-                    return 0;
+            var date_dim = ndx.dimension(dc.pluck('date'));
+            var minDate = date_dim.bottom(1)[0].date;
+            var maxDate = date_dim.top(1)[0].date;
+            function spend_by_name(name) {
+                return function(d) {
+                    if (d.name === name) {
+                        return d.rating;
+                    } else {
+                        return 0;
+                    }
                 }
             }
-        }
-        var jamieSpendByMonth = date_dim.group().reduceSum(spend_by_name('Jamie'));
-        var joshfSpendByMonth = date_dim.group().reduceSum(spend_by_name('Josh F')); 
-        var joshrSpendByMonth = date_dim.group().reduceSum(spend_by_name('Josh R')); var jamesSpendByMonth = date_dim.group().reduceSum(spend_by_name('James'));
-        var jackSpendByMonth = date_dim.group().reduceSum(spend_by_name('Jack'));
-        var seanSpendByMonth = date_dim.group().reduceSum(spend_by_name('Sean'));
-        var ralphSpendByMonth = date_dim.group().reduceSum(spend_by_name('Ralph'));
-        var alexSpendByMonth = date_dim.group().reduceSum(spend_by_name('Alex'));
-        var pavanSpendByMonth = date_dim.group().reduceSum(spend_by_name('Pavan'));
-        var tadghSpendByMonth = date_dim.group().reduceSum(spend_by_name('Tadgh'));
-        var coleSpendByMonth = date_dim.group().reduceSum(spend_by_name('Cole'));
-        var leeSpendByMonth = date_dim.group().reduceSum(spend_by_name('Lee'));
-        var marcusSpendByMonth = date_dim.group().reduceSum(spend_by_name('Marcus'));
-        var saadSpendByMonth = date_dim.group().reduceSum(spend_by_name('Saad'));
-        var paulSpendByMonth = date_dim.group().reduceSum(spend_by_name('Paul'));
-        var gusSpendByMonth = date_dim.group().reduceSum(spend_by_name('Gus'));
+            var jamieSpendByMonth = date_dim.group().reduceSum(spend_by_name('Jamie'));
+            var joshfSpendByMonth = date_dim.group().reduceSum(spend_by_name('Josh F')); 
+            var joshrSpendByMonth = date_dim.group().reduceSum(spend_by_name('Josh R')); var jamesSpendByMonth = date_dim.group().reduceSum(spend_by_name('James'));
+            var jackSpendByMonth = date_dim.group().reduceSum(spend_by_name('Jack'));
+            var seanSpendByMonth = date_dim.group().reduceSum(spend_by_name('Sean'));
+            var ralphSpendByMonth = date_dim.group().reduceSum(spend_by_name('Ralph'));
+            var alexSpendByMonth = date_dim.group().reduceSum(spend_by_name('Alex'));
+            var pavanSpendByMonth = date_dim.group().reduceSum(spend_by_name('Pavan'));
+            var tadghSpendByMonth = date_dim.group().reduceSum(spend_by_name('Tadgh'));
+            var coleSpendByMonth = date_dim.group().reduceSum(spend_by_name('Cole'));
+            var leeSpendByMonth = date_dim.group().reduceSum(spend_by_name('Lee'));
+            var marcusSpendByMonth = date_dim.group().reduceSum(spend_by_name('Marcus'));
+            var saadSpendByMonth = date_dim.group().reduceSum(spend_by_name('Saad'));
+            var paulSpendByMonth = date_dim.group().reduceSum(spend_by_name('Paul'));
+            var gusSpendByMonth = date_dim.group().reduceSum(spend_by_name('Gus'));
 
-        var compositeChart = dc.compositeChart('#composite-chart');
-        compositeChart 
-            .width(550)
-            .height(200)
-            .margins({top: 30, right: 50, bottom: 30, left: 50})
-            .dimension(date_dim)
-            .transitionDuration(1000)
-            .x(d3.time.scale().domain([minDate, maxDate]))
-            .yAxisLabel("Spend")
+            var compositeChart = dc.compositeChart('#composite-chart');
+            compositeChart 
+                .width($(goalBarChart.anchor()).parent().width())
+                .height(200)
+                .margins({top: 30, right: 50, bottom: 30, left: 50})
+                .dimension(date_dim)
+                .transitionDuration(1000)
+                .x(d3.time.scale().domain([minDate, maxDate]))
+                .yAxisLabel("Spend")
             
-            .legend(dc.legend().x(560).y(0).itemHeight(13).gap(5))
-            .renderHorizontalGridLines(true)
-            .compose([
-                dc.lineChart(compositeChart)
-                    .colors('green')
-                    .group(jamieSpendByMonth, 'Jamie'),
-                dc.lineChart(compositeChart)
-                    .colors('red')
-                    .group(joshfSpendByMonth, 'Josh F'),
-                dc.lineChart(compositeChart)
-                    .colors('blue')
-                    .group(joshrSpendByMonth, 'Josh R'),
-                dc.lineChart(compositeChart)
-                    .colors('maroon')
-                    .group(jackSpendByMonth, 'Jack'),
-                dc.lineChart(compositeChart)
-                    .colors('yellow')
-                    .group(jamesSpendByMonth, 'James'),                
-                dc.lineChart(compositeChart)
-                    .colors('pink')
-                    .group(seanSpendByMonth, 'Sean'),   
-                dc.lineChart(compositeChart)
-                    .colors('tomato')
-                    .group(ralphSpendByMonth, 'Ralph'),     
-                dc.lineChart(compositeChart)
-                    .colors('orange')
-                    .group(alexSpendByMonth, 'Alex'), 
-                dc.lineChart(compositeChart)
-                    .colors('dodgerblue')
-                    .group(pavanSpendByMonth, 'Pavan'), 
-                dc.lineChart(compositeChart)
-                    .colors('mediumseagreen')
-                    .group(tadghSpendByMonth, 'Tadgh'),  
-                dc.lineChart(compositeChart)
-                    .colors('grey')
-                    .group(coleSpendByMonth, 'Cole'),  
-                dc.lineChart(compositeChart)
-                    .colors('slateblue')
-                    .group(leeSpendByMonth, 'Lee'),  
-                dc.lineChart(compositeChart)
-                    .colors('violet')
-                    .group(marcusSpendByMonth, 'Marcus'),  
-                dc.lineChart(compositeChart)
-                    .colors('lightgrey')
-                    .group(leeSpendByMonth, 'Saad'),  
-                dc.lineChart(compositeChart)
-                    .colors('teal')
-                    .group(paulSpendByMonth, 'Paul'),  
-                dc.lineChart(compositeChart)
-                    .colors('black')
-                    .group(gusSpendByMonth, 'Gus'),  
-                
-            ])
+                .legend(dc.legend().x(560).y(0).itemHeight(13).gap(5))
+                .renderHorizontalGridLines(true)
+                .compose([
+                    dc.lineChart(compositeChart)
+                        .colors('green')
+                        .group(jamieSpendByMonth, 'Jamie'),
+                    dc.lineChart(compositeChart)
+                        .colors('red')
+                        .group(joshfSpendByMonth, 'Josh F'),
+                    dc.lineChart(compositeChart)
+                        .colors('blue')
+                        .group(joshrSpendByMonth, 'Josh R'),
+                    dc.lineChart(compositeChart)
+                        .colors('maroon')
+                        .group(jackSpendByMonth, 'Jack'),
+                    dc.lineChart(compositeChart)
+                        .colors('yellow')
+                        .group(jamesSpendByMonth, 'James'),                
+                    dc.lineChart(compositeChart)
+                        .colors('pink')
+                        .group(seanSpendByMonth, 'Sean'),   
+                    dc.lineChart(compositeChart)
+                        .colors('tomato')
+                        .group(ralphSpendByMonth, 'Ralph'),     
+                    dc.lineChart(compositeChart)
+                        .colors('orange')
+                        .group(alexSpendByMonth, 'Alex'), 
+                    dc.lineChart(compositeChart)
+                        .colors('dodgerblue')
+                        .group(pavanSpendByMonth, 'Pavan'), 
+                    dc.lineChart(compositeChart)
+                        .colors('mediumseagreen')
+                        .group(tadghSpendByMonth, 'Tadgh'),  
+                    dc.lineChart(compositeChart)
+                        .colors('grey')
+                        .group(coleSpendByMonth, 'Cole'),  
+                    dc.lineChart(compositeChart)
+                        .colors('slateblue')
+                        .group(leeSpendByMonth, 'Lee'),  
+                    dc.lineChart(compositeChart)
+                        .colors('violet')
+                        .group(marcusSpendByMonth, 'Marcus'),  
+                    dc.lineChart(compositeChart)
+                        .colors('lightgrey')
+                        .group(leeSpendByMonth, 'Saad'),  
+                    dc.lineChart(compositeChart)
+                        .colors('teal')
+                        .group(paulSpendByMonth, 'Paul'),  
+                    dc.lineChart(compositeChart)
+                        .colors('black')
+                        .group(gusSpendByMonth, 'Gus'),  
+                    
+                ])
             .brushOn(false)
             .render();
 
