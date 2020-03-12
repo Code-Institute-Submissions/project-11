34c9ef3954d12202b7899902be0471fac2e2e4f6
PeterKellett <peterwkellett@gmail.com>
2020-03-12 11:13:53 +0000

    Rendered all bar charts successfully.

--- js/main1.js
+++ js/main1.js
@@ -3,20 +3,20 @@ $(document).ready(function () {
 
     'use strict';
 
-    var attendance_Stacked_Chart = new dc.barChart('#attendance-chart');
+    const attendance_Stacked_Chart = new dc.barChart('#attendance-chart');
     //var compositeChart = new dc.compositeChart('#composite-chart');
-    var goals_Assists_Stacked_Chart = new dc.barChart('#goals-per-person-chart');
-    //var goalBarChart = new dc.barChart('#goals-per-opponent-chart');
-    //var total_goals_per_person = new dc.pieChart('#goals-per-person-piechart');
-    var total_goals_per_opponenet = new dc.pieChart('#goals-per-opponent-piechart');
+    const goals_Assists_Stacked_Chart = new dc.barChart('#goals-per-person-chart');
+    const goalBarChart = new dc.barChart('#goals-per-opponent-chart');
+    //const total_goals_per_person = new dc.pieChart('#goals-per-person-piechart');
+    //const total_goals_per_opponenet = new dc.pieChart('#goals-per-opponent-piechart');
 
 
     d3.json('data/stats.json').then(function (data) {
         console.log(data);
-        var dateFormatSpecifier = '%d/%m/%Y';
-        var dateFormat = d3.timeFormat(dateFormatSpecifier);
-        var dateFormatParser = d3.timeParse(dateFormatSpecifier);
-        var numberFormat = d3.format('.2f');
+        const dateFormatSpecifier = '%d/%m/%Y';
+        const dateFormat = d3.timeFormat(dateFormatSpecifier);
+        const dateFormatParser = d3.timeParse(dateFormatSpecifier);
+        const numberFormat = d3.format('.2f');
         //console.log(numberFormat);
 
         data.forEach(d => {
@@ -28,31 +28,25 @@ $(document).ready(function () {
         });
 
 
-        var ndx = crossfilter(data);
-        var all = ndx.groupAll();
+        const ndx = crossfilter(data);
+        const all = ndx.groupAll();
 
-        var name_dim = ndx.dimension(dc.pluck('name'));
-        var start = name_dim.group().reduceSum(function (d) {
+// attendance_Stacked_Chart = dc.barChart("#attendance-chart");
+        const name_dim = ndx.dimension(dc.pluck('name'));
+        const start = name_dim.group().reduceSum(function (d) {
             if (d.squad === 1) {
                 return +d.squad;
             } else {
                 return 0;
             }
         })
-        var sub = name_dim.group().reduceSum(function (d) {
+        const sub = name_dim.group().reduceSum(function (d) {
             if (d.squad === 0) {
                 return +d.squad + 1;
             } else {
                 return 0;
             }
         })
-
-        var name_dim = ndx.dimension(dc.pluck('name'));
-        var total_goals_per_person = name_dim.group().reduceSum(dc.pluck('goals'));
-        var total_assists_per_person = name_dim.group().reduceSum(dc.pluck('assists'));
-
-
-        //var attendance_Stacked_Chart = dc.barChart("#attendance-chart");
         attendance_Stacked_Chart
             .width($(attendance_Stacked_Chart.anchor()).parent().width())
             .margins({ top: 30, right: 50, bottom: 30, left: 70 })
@@ -62,17 +56,16 @@ $(document).ready(function () {
             .transitionDuration(1000)
             .x(d3.scaleBand())
             .xUnits(dc.units.ordinal)
-            .ordinalColors(['red', 'white'])
+            .ordinalColors(['blue', 'white'])
             .legend(dc.legend().x(0).y(10).itemHeight(15).gap(5))
 
+
         //Goals/assists per person chart
-        var name_dim = ndx.dimension(dc.pluck('name'));
-        var total_goals_per_person = name_dim.group().reduceSum(dc.pluck('goals'));
-        var total_assists_per_person = name_dim.group().reduceSum(dc.pluck('assists'));
-        //var goals_Assists_Stacked_Chart = dc.barChart('#goals-per-person-chart');
+        const total_goals_per_person = name_dim.group().reduceSum(dc.pluck('goals'));
+        const total_assists_per_person = name_dim.group().reduceSum(dc.pluck('assists'));
         goals_Assists_Stacked_Chart
             .width($(goals_Assists_Stacked_Chart.anchor()).parent().width())
-            //.height(200)
+            .height(200)
             .margins({ top: 30, right: 50, bottom: 30, left: 70 })
             .dimension(name_dim)
             .group(total_goals_per_person, "Goals")
@@ -84,6 +77,43 @@ $(document).ready(function () {
             .legend(dc.legend().x(0).y(10).itemHeight(15).gap(5))
         //.yAxis().ticks(4);
 
+
+        //Goals/asssists per opponent chart
+        const opponent_dim = ndx.dimension(dc.pluck('opponent'));
+        const total_goals_per_opponent = opponent_dim.group().reduceSum(dc.pluck('goals'));
+        const total_assists_per_opponent = opponent_dim.group().reduceSum(dc.pluck('assists'));
+        goalBarChart
+            .width($(goalBarChart.anchor()).parent().width())
+            .height(200)
+            .margins({ top: 30, right: 50, bottom: 30, left: 70 })
+            .dimension(opponent_dim)
+            .group(total_goals_per_opponent, "Goals")
+            .stack(total_assists_per_opponent, "Assists")
+            .transitionDuration(1000)
+            .x(d3.scaleBand())
+            .xUnits(dc.units.ordinal)
+            .ordinalColors(['blue', 'white'])
+            .legend(dc.legend().x(0).y(10).itemHeight(15).gap(5))
+        //.yAxis().ticks(4);
+
+
+
+        //Pie chart 1
+        //var name_dim = ndx.dimension(dc.pluck('name'));
+        /*
+        var total_goals_per_person = name_dim.group().reduceSum(dc.pluck('goals'));
+        dc.pieChart('#per-person-chart')
+            .width(150)
+            .height(150)
+            .dimension(name_dim)
+            .group(total_goals_per_person)
+            .transitionDuration(1000)
+        //.externalRadiusPadding(300)
+        //.externalLabels(true)
+        */
+
+
+        
         dc.renderAll();
     });
 });
